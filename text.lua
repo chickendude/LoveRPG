b073e2b1712b0449169a4725456bc12dc737ba45
Andreas Wilfer <andreas@wilfer.se>
2017-02-02 21:36:20 +0100

    added draw_box ...

--- text.lua
+++ text.lua
@@ -1,11 +1,17 @@
 -- file: text.lua - text routine
 
--- draw to (x, y)
--- wrap to x pos
---
 
--- put_string = love.graphics.printf
 function put_string (text, x, y , limit, align)
    align = align or "left"
    love.graphics.printf(text, x, y, limit, align)
 end
+
+--
+function draw_box(x, y, width, height, mode, colour)
+   mode = mode or "fill"
+   colour = colour or {255,255,0,255}
+   love.graphics.rectangle( mode, x, y, width, height)
+end
+
+--function draw_textbox(text, align_h, align_v)
+--end
