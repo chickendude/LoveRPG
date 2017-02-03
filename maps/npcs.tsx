d341f9bb72c906863cdfe71631682abd42db5526
chickendude <pokemaster103@gmail.com>
2017-02-03 20:26:47 +0800

    Forgot to add NPC sprites.

---
+++ maps/npcs.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="NPCs" tilewidth="16" tileheight="16" tilecount="24" columns="8">
+ <grid orientation="orthogonal" width="16" height="24"/>
+ <image source="NPCs.png" width="128" height="48"/>
+</tileset>
