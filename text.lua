9f470c2788fe3036926af61ad071fcbd1c56aa97
Andreas Wilfer <andreas@wilfer.se>
2017-02-04 16:09:41 +0100

    added colour option to draw_box

--- text.lua
+++ text.lua
@@ -6,8 +6,10 @@ function draw_text(text, x, y , limit, align)
   love.graphics.printf(text, x, y, limit, align)
 end
 
-function draw_box(x, y, width, height, mode)
+function draw_box(x, y, width, height, mode, rgba)
   mode = mode or "fill"
+  rgba = rgba or {10,10,230,255} -- some shade of blue
+  love.graphics.setColor(rgba)
   love.graphics.rectangle(mode, x, y, width, height)
 end
 
@@ -15,12 +17,14 @@ function draw_dialoguebox(text)
   text = text or " "
   local bh = 8*4
   local tw = camera.width - 16
-  draw_box(0,camera.height - bh ,camera.width, camera.height - bh)
+  local colour = {255,255,0,255}
+  draw_box(0,camera.height - bh ,camera.width, camera.height - bh, colour)
   draw_text(text, 0, camera.height - bh, tw)
 end
 
 function draw_menubox(menu)
   menu = menu or " "
-  mbw = 8*16
-  draw_box(camera.width-mbw, mbw, camera.height)
+  local colour = {0,0,0,255}
+  local mbw = 8*16
+  draw_box(camera.width-mbw, mbw, camera.height, colour)
 end
