fb51e49c23fa8e9d36368ec027c78e5ff81c5710
Andreas Wilfer <andreas@wilfer.se>
2017-02-04 15:22:06 +0100

    Fixed indentation to 2 spaces

--- text.lua
+++ text.lua
@@ -2,22 +2,22 @@
 
 
 function draw_text(text, x, y , limit, align)
-   align = align or "left"
-   love.graphics.printf(text, x, y, limit, align)
+  align = align or "left"
+  love.graphics.printf(text, x, y, limit, align)
 end
 
 function draw_box(x, y, width, height, mode)
-   mode = mode or "fill"
-   love.graphics.rectangle(mode, x, y, width, height)
+  mode = mode or "fill"
+  love.graphics.rectangle(mode, x, y, width, height)
 end
 
 function draw_dialoguebox(text)
-   local limit = camera.width - 20
-   local width = camera.width - 10
-   local height = 8 * 4
-   local x = 0
-   local y = camera.height - height
+  local limit = camera.width - 20
+  local width = camera.width - 10
+  local height = 8 * 4
+  local x = 0
+  local y = camera.height - height
 
-   draw_box(x,y,width,height)
-   draw_text(text,x,y,limit)
+  draw_box(x,y,width,height)
+  draw_text(text,x,y,limit)
 end
